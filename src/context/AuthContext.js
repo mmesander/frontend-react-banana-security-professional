904144b7a13efd353d274e037e96b386a80036cc
mmesander <mark_mesander@hotmail.com>
2023-06-13 18:23:44 +0200

    opdracht af

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,35 +1,55 @@
-import React, { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {createContext, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import jwt_decode from "jwt-decode";
 
 export const AuthContext = createContext({});
 
-function AuthContextProvider({ children }) {
-  const [isAuth, toggleIsAuth] = useState(false);
-  const navigate = useNavigate();
+function AuthContextProvider({children}) {
+    const [auth, setAuth] = useState({
+        isAuth: false,
+        user: null,
+    });
+    const navigate = useNavigate();
 
-  function login() {
-    console.log('Gebruiker is ingelogd!');
-    toggleIsAuth(true);
-    navigate('/profile');
-  }
+    function login(jwt_token) {
+        const decodedToken = jwt_decode(jwt_token);
+        localStorage.setItem('token', jwt_token)
+        // console.log(decodedToken)
+        setAuth({
+            ...auth,
+            isAuth: true,
+            user: {
+                email: decodedToken.email,
+                id: decodedToken.sub
+            }
+        });
+        console.log('Gebruiker is ingelogd!');
+        navigate('/profile');
+    }
 
-  function logout() {
-    console.log('Gebruiker is uitgelogd!');
-    toggleIsAuth(false);
-    navigate('/');
-  }
+    function logout() {
+        localStorage.removeItem('token');
+        setAuth({
+            ...auth,
+            isAuth: false,
+            user: null,
+        });
+        console.log('Gebruiker is uitgelogd!');
+        navigate('/');
+    }
 
-  const contextData = {
-    isAuth: isAuth,
-    login: login,
-    logout: logout,
-  };
+    const contextData = {
+        isAuth: auth.isAuth,
+        user: auth.user,
+        login: login,
+        logout: logout,
+    };
 
-  return (
-    <AuthContext.Provider value={contextData}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 export default AuthContextProvider;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,19 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from "axios";
 
 function SignUp() {
-  return (
-    <>
-      <h1>Registreren</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque eligendi
-        harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur deserunt
-        doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
-      <form>
-        <p>*Invoervelden*</p>
-      </form>
-      <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
-    </>
-  );
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:3000/register', {
+                email: email,
+                username: username,
+                password: password,
+            });
+            if (response.data) {
+                setEmail("");
+                setUsername("");
+                setPassword("");
+            }
+        } catch (e) {
+            console.error("Registratie mislukt!")
+        }
+    }
+
+    return (
+        <>
+            <h1>Registreren</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque
+                eligendi
+                harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur
+                deserunt
+                doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='reg-username-field'>
+                    Email
+                    <input
+                        type="email"
+                        name="reg-email"
+                        id="reg-email-field"
+                        placeholder="jouw@email.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label htmlFor='reg-username-field'>
+                    Gebruikersnaam
+                    <input
+                        type="text"
+                        name="reg-username"
+                        id="reg-username-field"
+                        placeholder="Gebruikersnaam"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <label htmlFor='reg-password-field'>
+                    Wachtwoord
+                    <input
+                        type="password"
+                        name="reg-password"
+                        id="reg-password-field"
+                        placeholder="Wachtwoord"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <button
+                    type="submit"
+                >
+                    Registreren
+                </button>
+            </form>
+            <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
+        </>
+    );
 }
 
 export default SignUp;
