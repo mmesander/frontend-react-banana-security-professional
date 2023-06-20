014f0a202c0eeec27aadde58fc671db95266d6b0
mmesander <mark_mesander@hotmail.com>
2023-06-20 14:25:35 +0200

    add profile data

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,35 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {AuthContext} from "../context/AuthContext";
+import axios from "axios";
 
 function Profile() {
     const {user} = useContext(AuthContext);
+    const [profileData, setProfileData] = useState({});
+
+    useEffect(() => {
+        async function fetchProfileData() {
+            const token = localStorage.getItem('token');
+
+            try {
+                const response = await axios.get('http://localhost:3000/660/private-content', {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+                setProfileData(response.data);
+                console.log(response.data)
+
+            } catch (e) {
+                console.error(e)
+            }
+        }
+
+        void fetchProfileData();
+
+    }, []);
+
 
     return (
         <>
@@ -13,11 +39,11 @@ function Profile() {
                 <p><strong>Gebruikersnaam:</strong> {user.username}</p>
                 <p><strong>Email:</strong> {user.email}</p>
             </section>
-            <section>
-                <h2>Strikt geheime profiel-content</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id
-                    molestias qui quo unde?</p>
-            </section>
+            {Object.keys(profileData).length > 0 &&
+                <section>
+                    <h2>{profileData.title}</h2>
+                    <p>{profileData.content}</p>
+                </section>}
             <p>Terug naar de <Link to="/">Homepagina</Link></p>
         </>
     );
